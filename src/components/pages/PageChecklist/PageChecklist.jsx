5099b0ecdcfda81aa703d1d2fa58b469788f2883
Rachel <rachel17.22@gmail.com>
2020-07-03 16:13:58 -0600

    delete button for removing entire lists. Full CRUD cycle

--- src/components/pages/PageChecklist/PageChecklist.jsx
+++ src/components/pages/PageChecklist/PageChecklist.jsx
@@ -1,10 +1,22 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
 import { useParams } from 'react-router-dom';
+import { useHistory } from 'react-router';
 import { db } from '../../../firebase';
+import { UserContext } from '../../../app/UserContextProvider';
 
 function PageChecklist() {
-  const [list, setList] = useState();
+  const { user } = useContext(UserContext);
+  const history = useHistory();
   const { id } = useParams();
+
+  const [list, setList] = useState();
+  const [checklistTitle, setChecklistTitle] = useState();
+  const [newChecklistItem, setNewChecklistItem] = useState();
 
   const getData = useCallback(
     async () => {
@@ -15,9 +27,34 @@ function PageChecklist() {
 
       const listItems = response.data();
       setList(listItems);
+      setChecklistTitle(listItems.title);
     },
     [id],
   );
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+
+    const updateData = {
+      title: checklistTitle,
+      createdBy: user.uid,
+      items: list,
+    };
+
+    await db.collection('checklists')
+      .doc(id)
+      .set(updateData);
+  };
+
+  const handleDelete = async (evt) => {
+    evt.preventDefault();
+
+    await db.collection('checklists')
+      .doc(id)
+      .delete();
+
+    history.push('/');
+  };
 
   useEffect(() => {
     getData();
@@ -30,12 +67,49 @@ function PageChecklist() {
   }
   return (
     <div>
-      <h2>{list.title}</h2>
-      <ul>
-        {list.items.map((item) => (
-          <li>{item}</li>
-        ))}
-      </ul>
+      <form
+        onSubmit={handleSubmit}
+      >
+        <h2>
+          <input
+            value={checklistTitle}
+            onChange={(evt) => {
+              setChecklistTitle(evt.target.value);
+            }}
+          />
+        </h2>
+
+        <ul>
+          {list.items.map((item) => (
+            <li>
+              <input
+                value={item}
+                onChange={(evt) => {
+                  setList(evt.target.value);
+                }}
+              />
+            </li>
+          ))}
+          <li>
+            <input
+              placeholder="new item"
+              value={newChecklistItem}
+              onChange={(evt) => {
+                setNewChecklistItem(evt.target.value);
+              }}
+            />
+          </li>
+        </ul>
+        <button type="submit">
+          Update
+        </button>
+      </form>
+      <button
+        type="button"
+        onClick={handleDelete}
+      >
+        Delete
+      </button>
     </div>
   );
 }
